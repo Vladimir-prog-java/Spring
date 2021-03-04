16f83ccfc8cec2186900b9ff54cbb385df62b34c
Vladimir <300wrongaway@mail.ru>
2021-03-04 21:17:37 +0300

    Refactor 2

--- src/main/java/ru/itsjava/service/ServiceMenuImpl.java
+++ src/main/java/ru/itsjava/service/ServiceMenuImpl.java
@@ -6,8 +6,11 @@ import ru.itsjava.App;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class ServiceMenuImpl implements ServiceMenu {
 
@@ -15,12 +18,22 @@ public class ServiceMenuImpl implements ServiceMenu {
     @SneakyThrows
     public void menu(String fileName) {
         // try (BufferedReader menu = Files.newBufferedReader(Paths.get(fileName))) {
-        InputStream menu = Files.class.getResourceAsStream("src/main/resources/price.txt");
 
-//        String text;
-//        while ((text = menu) != null) {
-//            System.out.println(text);
-//        }
+    ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("/price.txt");
+       // Scanner s = new Scanner(inputStream);
+    //    String menu = s.toString();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String menu = bufferedReader.toString();
+
+//        String data = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+//        InputStream menu = ServiceMenu.class.getResourceAsStream(fileName);
+//        Properties properties = new Properties();
+//        properties.load(menu);
+        String text;
+        while ((text = menu) != null) {
+            System.out.println(text);
+        }
 
 //        InputStream in = Test.class.getResourceAsStream("app.properties");
 //
