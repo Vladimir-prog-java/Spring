9b28a9549996c9668e3924b12ce0e4c5e8c78082
Vladimir <300wrongaway@mail.ru>
2021-03-10 18:47:42 +0300

    Java-based-configuration add Autowired

--- java-based-configuration/src/main/java/ru/itsjava/service/ServiceCoffeeImpl.java
+++ java-based-configuration/src/main/java/ru/itsjava/service/ServiceCoffeeImpl.java
@@ -1,13 +1,17 @@
 package ru.itsjava.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.itsjava.domain.Coffee;
 
-@RequiredArgsConstructor
 public class ServiceCoffeeImpl implements ServiceCoffee {
     private final ServiceMenu menuService;
     private final ServiceScanner scannerService;
+@Autowired
+    public ServiceCoffeeImpl(ServiceMenu menuService, ServiceScanner scannerService) {
+        this.menuService = menuService;
+        this.scannerService = scannerService;
+    }
 
     @SneakyThrows
     @Override
