c9cf4c604f91365febe7fca6e4c97a08bc7b4c83
Vladimir <300wrongaway@mail.ru>
2021-03-10 18:26:56 +0300

    Annotation-based-configuration

---
+++ annotation-based-configuration/src/main/java/ru/itsjava/App.java
@@ -0,0 +1,15 @@
+package ru.itsjava;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import ru.itsjava.service.ServiceCoffeeImpl;
+
+@ComponentScan ({"ru.itsjava"})
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        context.getBean(ServiceCoffeeImpl.class).getCoffee();
+
+    }
+}
---
+++ annotation-based-configuration/src/main/java/ru/itsjava/service/ServiceMenuImpl.java
@@ -0,0 +1,24 @@
+package ru.itsjava.service;
+
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+@Service
+public class ServiceMenuImpl implements ServiceMenu {
+
+    @Override
+    @SneakyThrows
+    public void menu(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String text;
+        while ((text = bufferedReader.readLine()) != null) {
+            System.out.println(text);
+        }
+    }
+}
+
